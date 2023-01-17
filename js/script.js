2f2f54e8b2a3ce3ec5c9221b6af94e12b90bba71
YaroslavIl <yaroslavovich90@gmail.com>
2023-01-17 17:10:40 +0200

    p3

--- js/script.js
+++ js/script.js
@@ -3,16 +3,16 @@
 
 let slideItem = [
     {
-        src: '../img/1.jpg',
+        src: 'img/1.jpg',
     },
     {
-        src: '../img/2.jpg',
+        src: 'img/2.jpg',
     },
     {
-        src: '../img/3.jpg',
+        src: 'img/3.jpg',
     },
     {
-        src: '../img/4.jpg',
+        src: 'img/4.jpg',
     },
 ];
 let boxSlid = document.querySelector('.slider');
